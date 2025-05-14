ac939cd22b6d5b8c56ea6f1ea8b43928f3ab9572
ojkaas <olaf@ojkaas.nl>
2025-05-14 10:32:24 +0200

    disable energy report, as it gives problems for users with climate devices

--- pymelcloud/device.py
+++ pymelcloud/device.py
@@ -84,8 +84,10 @@ class Device(ABC):
             and c.get("BuildingID") == self.building_id
         )
         self._state = await self._client.fetch_device_state(self)
-        if(self.device_type != DEVICE_TYPE_ERV): 
-            self._energy_report = await self._client.fetch_energy_report(self)
+
+        # At the moment this crashes the climate function, just remove the whole energy report for now.
+        #if(self.device_type != DEVICE_TYPE_ERV): 
+        #    self._energy_report = await self._client.fetch_energy_report(self)
 
         if self._device_units is None and self.access_level != ACCESS_LEVEL.get(
             "GUEST"
